63e2038ce27f8938c57de44b9df7eae537deb0d3
lin2724 <lin2724@msn.com>
2016-11-20 22:47:32 +0800

    add datebase

--- sqlite_test.py
+++ sqlite_test.py
@@ -1,1 +1,26 @@
 import sqlite3
+import os
+import datetime
+import time
+
+db_path = os.path.join('other', 'test.db')
+print db_path
+
+
+if not os.path.exists(db_path):
+    con = sqlite3.connect(db_path)
+    con.execute('CREATE TABLE url_record(\
+      url CHAR PRIMARY KEY ,\
+      count INT DEFAULT 0,\
+      desciption CHAR ,\
+      is_done INT DEFAULT 0,\
+      record_time DATE )')
+    con.commit()
+else:
+    con = sqlite3.connect(db_path)
+    now = datetime.datetime.now()
+    print now
+    con.execute('INSERT INTO url_record VALUES (?,?,?,?,?)', ('httpxx', 1, 'hello',1 ,now))
+    con.commit()
+con.close()
+
